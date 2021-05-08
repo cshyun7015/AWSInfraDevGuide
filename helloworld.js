97fd24ecbd08fdc483630fe06082c9137405cdda
cshyun7015 <44148115+cshyun7015@users.noreply.github.com>
2021-05-08 21:34:08 +0900

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World.. CodeDeploy Testing...\n')
+   response.end('Hello World.. Blue/Green Testing...\n')
 }).listen(3000)
 
 // Console will print the message
