0deba4e9e125c7a06c01a10c128743daae07210e
cshyun7015 <44148115+cshyun7015@users.noreply.github.com>
2021-05-08 13:38:14 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-    
+
 [[ -e /etc/init/helloworld.conf ]] \
   && status helloworld | \
     grep -q '^helloworld start/running, process' \
   && [[ $? -eq 0 ]] \
   && stop helloworld || echo "Application not started"  
+
+rm /usr/local/helloworld/helloworld.js
+rm /etc/init/helloworld.conf
